b8ac57854a5c65829dbdd7794d21da5db57e6357
Asher <asher@beeriprint.co.il>
2024-03-05 08:30:56 +0200

    add xdebug support

--- init.lua
+++ init.lua
@@ -96,7 +96,7 @@ vim.g.maplocalleader = ' '
 --  For more options, you can see `:help option-list`
 --
 vim.opt.guicursor = 'n-v-i-c:block-Cursor'
-vim.opt.colorcolumn = "120"
+vim.opt.colorcolumn = '120'
 
 -- Make line numbers default
 vim.opt.number = true
@@ -257,8 +257,80 @@ require('lazy').setup {
     },
   },
 
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
     'tpope/vim-fugitive',
+  },
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require 'dap'
+      dap.adapters.php = {
+        type = 'executable',
+        command = 'node',
+        args = { os.getenv 'HOME' .. '/php-debug/out/phpDebug.js' },
+      }
+      dap.configurations.php = {
+        {
+          type = 'php',
+          request = 'launch',
+          name = 'Listen for Xdebug',
+          port = 9003,
+          pathMappings = {
+            ['/var/www/html'] = '${workspaceFolder}/application',
+          },
+        },
+      }
+      vim.keymap.set('n', '<F5>', function()
+        dap.continue()
+      end)
+      vim.keymap.set('n', '<F10>', function()
+        dap.step_over()
+      end)
+      vim.keymap.set('n', '<F11>', function()
+        dap.step_into()
+      end)
+      vim.keymap.set('n', '<F12>', function()
+        dap.step_out()
+      end)
+      vim.keymap.set('n', '<leader>b', function()
+        dap.toggle_breakpoint()
+      end)
+      vim.keymap.set('n', '<leader>B', function()
+        dap.set_breakpoint()
+      end)
+      vim.keymap.set('n', '<leader>lp', function()
+        dap.set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+      end)
+      vim.keymap.set('n', '<leader>dr', function()
+        dap.repl.open()
+      end)
+      vim.keymap.set('n', '<leader>dl', function()
+        dap.run_last()
+      end)
+      vim.keymap.set({ 'n', 'v' }, '<leader>dh', function()
+        require('dap.ui.widgets').hover()
+      end)
+      vim.keymap.set({ 'n', 'v' }, '<leader>dp', function()
+        require('dap.ui.widgets').preview()
+      end)
+      vim.keymap.set('n', '<leader>df', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.frames)
+      end)
+      vim.keymap.set('n', '<Leader>ds', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.scopes)
+      end)
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+  },
+  {
+    'theHamsta/nvim-dap-virtual-text',
+  },
+  {
+    'nvim-telescope/telescope-dap.nvim',
   },
 
   -- NOTE: Plugins can also be configured to run lua code when they are loaded.
