b1395bc62ff2b6ae95fb77642999b077ccc71bde
Asher <asher@beeriprint.co.il>
2024-06-06 09:42:28 +0300

    change gi keybind

--- lua/ashulamas/lazy_plugins/lsp.lua
+++ lua/ashulamas/lazy_plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
 
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
